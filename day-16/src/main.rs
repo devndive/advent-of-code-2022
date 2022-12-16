5d16f202709d45f9fb9e2ba5f0205365a9ed02a5
Yann Duval <yann.duval@posteo.de>
2022-12-17 00:18:37 +0100

    There is a small bug in day 16. Basics are working

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,4 +1,8 @@
-use std::{collections::{HashMap, HashSet}, fs, hash::Hash, vec};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet, VecDeque},
+    fs, vec,
+};
 
 use regex::Regex;
 
@@ -19,13 +23,41 @@ impl Valve {
     }
 }
 
-#[derive(Eq, PartialEq, Hash, Clone)]
-struct Node {
-    name: String,
-    released_steam: u32,
-    minutes_remaining: u32,
-    is_open: bool,
-    leafes: Vec<(Node, u32)>,
+fn find_routes(
+    start_point: &String,
+    /*end_point: &String,*/
+    valves: &HashMap<String, Valve>,
+) -> HashMap<String, u32> {
+    let mut distances: HashMap<String, u32> = HashMap::new();
+    println!("{} -> ", start_point);
+
+    let mut que = VecDeque::new();
+    let mut visited: HashSet<String> = HashSet::new();
+
+    visited.insert(start_point.clone());
+    que.push_back(start_point);
+
+    let mut distance = 0;
+
+    while !que.is_empty() {
+        let item = que.pop_front().unwrap();
+        //if item == end_point {
+        //    break;
+        //}
+
+        distance += 1;
+        for valve in valves.get(item).unwrap().nodes.iter() {
+            if !visited.contains(valve) {
+                println!("      {}: {}", valve, distance);
+                visited.insert(valve.clone());
+
+                distances.insert(valve.clone(), distance);
+                que.push_back(valve);
+            }
+        }
+    }
+
+    return distances;
 }
 
 fn main() {
@@ -36,104 +68,136 @@ fn main() {
         Regex::new(r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z,\s]*)")
             .unwrap();
 
-    let mut map: HashMap<String, Valve> = HashMap::new();
+    let mut valves: HashMap<String, Valve> = HashMap::new();
     for room in rooms {
         let matches = re.captures(room).unwrap();
 
-        map.insert(
+        valves.insert(
             matches[1].to_string(),
             Valve::from(&matches[2], &matches[3]),
         );
     }
 
-    /*
-    let root_node = Node {
-        name: String::from("AA"),
-        leafes: HashMap::new(),
-    };
-    */
+    // Build two HashMaps to hold oll possible ways from each to each point
+    let mut distances: HashMap<String, HashMap<String, u32>> = HashMap::new();
 
-    let mut total_steam = 0;
-    let mut nodes_visited_and_open = HashMap::new();
-    nodes_visited_and_open.insert(String::from("AA"), Node {
-        name: String::from("AA"),
-        released_steam: 0,
-        is_open: false,
-        minutes_remaining: 30,
-        leafes: vec![]
-    });
+    for v in valves.iter() {
+        // ignore all paths to valves that do not add value (release pressure)
+        if v.1.flow_rate == 0 && v.0 != &String::from("AA") {
+            continue;
+        }
 
-    let root_node = process_leaf(&String::from("AA"), &map, 30, nodes_visited_and_open.clone());
-    let mut cur_node = root_node.0;
-
-    while cur_node.leafes.len() > 0 {
-        let highest = cur_node.leafes.iter().max_by(|x, y| x.1.cmp(&y.1)).unwrap();
-        println!("{}: {}", highest.0.name, highest.0.minutes_remaining);
-
-        total_steam = total_steam + highest.0.released_steam;
-
-        cur_node = highest.0.clone();
+        distances.insert(v.0.clone(), find_routes(&v.0.clone(), &valves));
     }
 
-    println!("Part 1: {}", total_steam);
+    let open_valves: HashSet<String> = HashSet::new();
 
+    let res = process_leaf(String::from("AA"), 10, &valves, distances, open_valves);
+    println!(
+        "Part 1: {} - {}",
+        res.0,
+        res.1.join(",")
+    );
 }
 
 fn process_leaf(
-    node_name: &String,
-    map: &HashMap<String, Valve>,
-    minutes_left: u32,
-    mut nodes_visited: HashMap<String, Node>
-) -> (Node, u32) {
-    let node = map.get(node_name).unwrap();
-    let mut minutes_remaining = minutes_left;
-
-    let mut released_steam = 0;
-    // let mut open_valve_time_cost = 0;
-    if node.flow_rate > 0 && minutes_remaining > 1 {
-        // open_valve_time_cost = 1;
-        released_steam = node.flow_rate * (minutes_left - 1);
-        minutes_remaining -= 1;
+    leaf: String,
+    mut remaining_time: u32,
+    valves: &HashMap<String, Valve>,
+    distances: HashMap<String, HashMap<String, u32>>,
+    mut open_valves: HashSet<String>,
+) -> (u32, Vec<String>) {
+        let valve = valves.get(&leaf).unwrap();
+    if open_valves.contains(&leaf)  || valve.flow_rate == 0 {
+        println!("------------- should not have done this");
     }
-
-    let mut result_node = Node {
-        name: node_name.clone(),
-        is_open: node.flow_rate > 0 && released_steam > 0,
-        released_steam: released_steam,
-        minutes_remaining: minutes_remaining,
-        leafes: vec![],
-    };
-
-    nodes_visited.insert(node_name.clone(), result_node.clone());
-
-    if minutes_remaining > 1 {
-        let mut max_released_steam = 0;
-        // let ret = vec![];
-        for leaf in node.nodes.iter() {
-            // let leaf_node = map.get(leaf).unwrap();
-            // let released_steam = leaf_node.flow_rate * (minutes_left - 1 - open_valve_time_cost);
-
-            let visited = nodes_visited.get(leaf);
-            if visited.is_none() {
-                let next_leaf_released_steam =
-                    process_leaf(leaf, map, minutes_remaining - 1, nodes_visited.clone());
-                // ret.push((leaf, next_leaf_released_steam));
-
-                max_released_steam = max_released_steam.max(next_leaf_released_steam.1);
-                result_node
-                    .leafes
-                    .push((next_leaf_released_steam.0.clone(), next_leaf_released_steam.1));
-            } else {
-                result_node
-                    .leafes
-                    .push((visited.unwrap().clone(), 0));
-            }
-
-        }
-    nodes_visited.insert(node_name.clone(), result_node.clone());
-
-        return (result_node, max_released_steam + released_steam);
+    
+    if remaining_time < 2 {
+        // we have no more time todo anything meaningful here
+        println!("Early return - 0");
+        return (0, vec![leaf]);
+    } else if remaining_time == 2 {
+        // Open valve and let it run, no more need to go anywhere
+        println!("Early return - {}", valve.flow_rate);
+        return (valve.flow_rate, vec![leaf]);
     } else {
-        return (result_node, released_steam);
+        // open the valve
+
+        let mut released_steam = valve.flow_rate;
+        if valve.flow_rate > 0 {
+            remaining_time -= 1;
+
+            println!(
+                "{}: {} * {} = {}",
+                leaf,
+                remaining_time,
+                valve.flow_rate,
+                remaining_time * valve.flow_rate
+            );
+            released_steam = remaining_time * valve.flow_rate;
+            open_valves.insert(leaf.clone());
+        }
+
+        // get distances to valves
+        let distances_form_leaf = distances.get(&leaf);
+
+        // No place to go from here
+        if distances_form_leaf.is_none() {
+            println!("No places to go: {}", released_steam);
+            return (released_steam, vec![leaf]);
+        }
+        let distances_form_leaf = distances_form_leaf.unwrap();
+
+        //
+        //print!("{} ({}) ->", leaf, remaining_time);
+        let mut next_valves = vec![];
+        for (next_valve, distance) in distances_form_leaf.iter() {
+            //print!(" {}", next_valve);
+            let next_valve_node = valves.get(next_valve).unwrap();
+
+            //let distance_to_next_leaf = distances_form_leaf.get(next_valve).unwrap();
+
+            println!("{} => {} - {} - {}", leaf, next_valve, distance, remaining_time);
+            // if the distance to next leaf is greater or equal to the time remaining no need to go there
+            // if the valve is already open, no need to go there
+            if *distance <= remaining_time
+                && !open_valves.contains(next_valve)
+                && next_valve_node.flow_rate > 0
+            {
+                let res = process_leaf(
+                    next_valve.clone(),
+                    remaining_time - distance,
+                    valves,
+                    distances.clone(),
+                    open_valves.clone(),
+                );
+                //println!(
+                //    "Remaining time: {} - {} - {}",
+                //    remaining_time - distance, next_valve, value
+                //);
+                if res.0 > 0 {
+                    next_valves.push((next_valve, res));
+                }
+            }
+        }
+
+        next_valves.iter().for_each(|(next_valve, value)| {
+            println!("Options: {} -> {} | {}", leaf, next_valve, value.0);
+        });
+
+        let most_valuable_step = next_valves.iter().max_by(|x, y| x.1.cmp(&y.1));
+        if most_valuable_step.is_some() {
+            let most_valuable_step = most_valuable_step.unwrap();
+            println!(
+                "  Chosen: {} -> {} | {}",
+                leaf, most_valuable_step.0, most_valuable_step.1.0
+            );
+            let mut ret_vec = (most_valuable_step.1).1.clone();
+            ret_vec.push(format!("{} ({})", leaf, most_valuable_step.1.0));
+            return (released_steam + most_valuable_step.1.0, ret_vec);
+        }
+
+        println!("Released steam {}", released_steam);
+        return (released_steam, vec![format!("{} (.{})",leaf, released_steam)]);
     }
 }
