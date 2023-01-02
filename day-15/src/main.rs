9e54800c78f68f732249508b1b555c10f4684779
Yann Duval <yann.duval@posteo.de>
2023-01-02 15:46:51 +0100

    Limit number of tests

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,6 +1,36 @@
+#[cfg(test)]
+mod tests {
+    use crate::{calculate_points, Pair, Position};
+
+    #[test]
+    fn simple_test() {
+        let p = Pair {
+            sensor: Position { x: 0, y: -3 },
+            distance: 7,
+        };
+
+        let points = calculate_points(&p);
+
+        for y in -10..=10 {
+            for x in -10..=10 {
+                if points.contains(&Position { x, y }) {
+                    print!("x");
+                } else {
+                    print!(".");
+                }
+            }
+
+            println!("");
+        }
+
+        assert!(false);
+    }
+}
+
 use std::{
+    collections::{HashMap, HashSet},
     fs,
-    thread::{self, Thread},
+    thread::{self},
 };
 
 use regex::Regex;
@@ -16,6 +46,33 @@ struct Pair {
     // beacon: Position,
     sensor: Position,
     distance: i32,
+}
+
+fn calculate_points(p: &Pair) -> Vec<Position> {
+    let mut points_on_circle = vec![];
+
+    for x in p.sensor.x - p.distance..=p.sensor.x + p.distance {
+        // (x1 - x2).abs() + (y1 - y2).abs() = distance
+        // x1 = p.sensor.x
+        // y1 = p.sensor.y
+        // x2 = x
+        // y2 = ?
+        // (0 - -5) + (0 - y2) = 5
+        // => 5     + (-y2).abs() = 5
+        // => y2 = 0
+
+        // (0 - -4) + (0 - y2) = 5
+        // 4 + (-y2).abs() = 5
+        // y2 = 1
+
+        let x_diff = (p.sensor.x - x).abs();
+        let y = p.distance - x_diff;
+
+        points_on_circle.push(Position { x: x, y: y });
+        points_on_circle.push(Position { x: x, y: y * -1 });
+    }
+
+    return points_on_circle;
 }
 
 impl Position {
@@ -39,7 +96,6 @@ impl Pair {
 
         Self {
             sensor,
-            // beacon,
             distance: (sensor.x - beacon.x).abs() + (sensor.y - beacon.y).abs(),
         }
     }
@@ -117,53 +173,170 @@ fn main() {
         .map(|row| Pair::from(row))
         .collect::<Vec<Pair>>();
 
-    let lb = 0;
-    let rb = 4000000;
-
-    let range = (lb..=rb).collect::<Vec<i32>>();
-
-    let windows = range.chunks((rb / 10) as usize);
-    println!("Chunks: {} ", windows.len());
-
-    let mut handles = vec![];
-    for window in windows {
-        println!("Hä");
-        let p = pairs.clone();
-        let owned_window = window.to_vec();
-
-        let handle = thread::spawn(move || {
-            println!("Thread - start {}", owned_window.len());
-            for y in owned_window {
-                println!("y: {}", y);
-                for x in lb..=rb {
-                    let pos = Position { x, y: y.clone() };
-
-                    let any_in_range = p.iter().any(|p| p.is_in_range(&pos));
-                    if !any_in_range {
-                        panic!("Found ya! ({}/{})", x, y);
-                    }
-                }
-            }
-        });
-
-        handles.push(handle);
+    let mut all_points = vec![];
+    for p in pairs.iter() {
+        let mut points = calculate_points(p);
+        all_points.append(&mut points);
     }
 
-    for h in handles {
-        h.join().unwrap();
+    println!("{}", all_points.len());
+    let mut min_x = 4000000;
+    let mut max_x = 0;
+    let mut min_y = 4000000;
+    let mut max_y = 0;
+
+    for point in all_points.iter() {
+        min_x = min_x.min(point.x);
+        max_x = max_x.max(point.x);
+        min_y = min_y.min(point.y);
+        max_y = max_y.max(point.y);
     }
+
+    if min_x < 0 {
+        min_x = 0;
+    }
+
+    if max_x > 4000000 {
+        max_x = 4000000;
+    }
+
+    if min_y < 0 {
+        min_y = 0;
+    }
+
+    if max_y > 4000000 {
+        max_y = 4000000;
+    }
+
+    println!("X: {min_x} - {max_x}");
+    println!("Y: {min_y} - {max_y}");
 
     /*
-    for y in lb..=rb {
-        println!("y: {}", y);
-        for x in lb..=rb {
-            let pos = Position { x, y };
+    for y in -15..=15 {
+        for x in -40..=40 {
+            if all_points.contains(&Position { x, y }) {
+                print!("x");
+            } else {
+                print!(".");
+            }
+        }
 
-            let any_in_range = pairs.iter().any(|p| p.is_in_range(&pos));
-            if !any_in_range {
-                panic!("Found ya! ({}/{})", x, y);
+        println!("");
+    }
+    */
+
+    /*
+    let mut hash_map = vec![];
+    for point in all_points {
+        let pt = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+
+        pt.iter().for_each(|t| {
+            let x_1 = point.x + t.0;
+            let y_1 = point.y + t.1;
+
+            hash_map.push(Position { x: x_1, y: y_1 });
+        });
+    }
+ */
+    /*
+    for y in -15..=15 {
+        for x in -40..=40 {
+            if hash_map.contains(&Position { x, y }) {
+                print!("x");
+            } else {
+                print!(".");
+            }
+        }
+
+        println!("");
+    } */
+
+    //println!("{}", hash_map.len());
+    /*
+    hash_map.iter().for_each(|p_check| {
+        //if p_check.x >= lb && p_check.x <= rb && p_check.y >= lb && p_check.y <= rb {
+        /*
+        if p_check.x <= 20 && p_check.y <= 20 {
+            if !pairs.iter().any(|p| p.is_in_range(p_check)) {
+                println!("Found you: {:?}", p_check);
+            }
+        }
+             */
+
+        let pt = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ];
+
+        let mut result = false;
+        if p_check.x <= 20 && p_check.y <= 20 {
+            result = pt.iter().all(|test| {
+                return hash_map.contains(&Position {
+                    x: p_check.x + test.0,
+                    y: p_check.y + test.1,
+                });
+            });
+        }
+
+        if result {
+            println!("Found you {:?}", p_check);
+        }
+    }); */
+
+    for y in min_y-1..=max_y+1 {
+        println!("{y}");
+
+    //for point_check in hash_map.iter() {
+    //    if point_check.x == 14 {
+    //        println!("Checking {}/{}", point_check.x, point_check.y);
+    //    }
+        for x in min_x-1..=max_x+1 {
+            if !pairs
+                .iter()
+                .any(|pair| pair.is_in_range(&Position { x, y }))
+            {
+                let pt = [
+                    (-1, -1),
+                    (0, -1),
+                    (1, -1),
+                    (-1, 0),
+                    (1, 0),
+                    (-1, 1),
+                    (0, 1),
+                    (1, 1),
+                ];
+
+                let result = pt.iter().all(|test| {
+
+                    return pairs.iter().any(|pair| {
+                        return pair.is_in_range(&Position {
+                            x: x + test.0,
+                            y: y + test.1,
+                        });
+                    });
+                });
+
+                //let point_check_out_of_range = pairs.iter().any(|pair| pair.is_in_range(&point_check));
+
+                if result /*&& !point_check_out_of_range*/ {
+                    println!("Found {}/{}", x, y);
+                }
             }
         }
     }
-    */
 }
